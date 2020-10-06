119f8d4ad0b0d5b2f5044b7ddfb8da659d07b6e8
Nikolay Nechaev <nikolay_nechaev@mail.ru>
2020-10-06 20:22:16 +0300

    Added first simple version of a website based on gorilla/mux

---
+++ src/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"html/template"
+	"net/http"
+	"os"
+)
+
+var tmpl *template.Template
+
+func handleRoot(w http.ResponseWriter, _ *http.Request) {
+	if err := tmpl.ExecuteTemplate(w, "/", nil); err != nil {
+		panic(err)
+	}
+}
+
+func loadHtmlTemplates() (*template.Template, error) {
+	if err := os.Chdir("src/templates"); err != nil {
+		return nil, err
+	}
+	defer os.Chdir("../..")
+
+	tmpl := template.New("HTML templates")
+	return tmpl.ParseFiles("index.html")
+}
+
+func init() {
+	var err error
+
+	tmpl, err = loadHtmlTemplates()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handleRoot)
+
+	listenAddr := "localhost:4000"
+	fmt.Println("Listening at http://" + listenAddr)
+	panic(http.ListenAndServe(listenAddr, r))
+}
