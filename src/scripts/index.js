d456f7d22aadbfee17a0783d38787b98e72f214e
Tatiana Kadykova <tanya-kta@bk.ru>
2020-11-22 18:42:03 +0300

    Connected to API; changed dates comparison

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,41 +1,8 @@
-//import axios from 'axios'
-
 var day_buttons = new Vue({
     el:'#day_buttons',
 
     data: {
-        days: [
-            {
-                id: 1,
-                date: "2020-11-18T21:00:00.000Z",
-                average_color: [255, 0, 0]
-            },
-            {
-                id: 2,
-                date: "2020-11-21T21:00:00.000Z",
-                average_color: [255, 0, 0]
-            },
-            {
-                id: 1,
-                date: "2020-10-23T21:00:00.000Z",
-                average_color: [255, 0, 0]
-            },
-            {
-                id: 1,
-                date: "2020-10-30T21:00:00.000Z",
-                average_color: [255, 0, 0]
-            },
-            {
-                id: 1,
-                date: "2020-11-12T21:00:00.000Z",
-                average_color: [255, 0, 0]
-            },
-            {
-                id: 1,
-                date: "2020-11-19T21:00:00.000Z",
-                average_color: [255, 0, 0]
-            }
-        ]
+        days: []
     },
 
     methods: {
@@ -54,7 +21,7 @@ var day_buttons = new Vue({
         isDayFilled: function (day) {
             for(let i = 0; i < this.days.length; ++i)
             {
-                if(this.days[i].date === day.toISOString())
+                if(Date.parse(this.days[i].date) === Date.parse(day.toISOString()))
                     return true;
             }
             return false;
@@ -63,7 +30,7 @@ var day_buttons = new Vue({
         getFilledDayColor: function (day) {
             for(let i = 0; i < this.days.length; ++i)
             {
-                if(this.days[i].date === day.toISOString())
+                if(Date.parse(this.days[i].date) === Date.parse(day.toISOString()))
                     return this.days[i].average_color;
             }
             return [169, 169, 169];
@@ -71,27 +38,26 @@ var day_buttons = new Vue({
 
         getDisplayedWeeks: function () {
             let closest_sunday = new Date();
-            closest_sunday.setDate(closest_sunday.getDate() + 7 - closest_sunday.getDay());
+            closest_sunday = new Date(Date.UTC(closest_sunday.getFullYear(), closest_sunday.getMonth(),
+                closest_sunday.getDate() + 7 - closest_sunday.getDay()));
             let three_weeks = [];
             for(let i = 2; i >= 0; --i)
             {
                 let current_week = [];
                 for(let j = 6; j >= 0; --j)
                 {
-                    current_week.push(new Date());
-                    current_week[6 - j].setDate(closest_sunday.getDate() - j - i * 7);
-                    current_week[6 - j].setHours(0, 0, 0, 0);
+                    current_week.push(new Date(closest_sunday));
+                    current_week[6 - j].setUTCDate(closest_sunday.getUTCDate() - j - i * 7);
                 }
                 three_weeks.push(current_week);
             }
             return three_weeks;
         }
-    }
-    /*
+    },
     mounted() {
         axios.get("/api/days/brief").then(response => {
             this.days = response.data;
         })
-    }*/
+    }
 
 })
