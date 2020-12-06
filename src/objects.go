7f97693781c338e9d9981f188712fcddedcaa96c
Nikolay Nechaev <nikolay_nechaev@mail.ru>
2020-12-06 19:29:52 +0300

    Added `WhetherActivityOrEmotion` type and the corresponding value to `ActivityOrEmotionType`

--- src/objects.go
+++ src/objects.go
@@ -5,6 +5,23 @@ import (
 	"time"
 )
 
+// Helpers
+
+// WhetherActivityOrEmotion is technically a synonym for `string`, but the objects of this type should only take values
+// from the set of the following constants: `EntityTypeActivity`, `EntityTypeEmotion`
+type WhetherActivityOrEmotion string
+
+const (
+	// EntityTypeActivity is a constant representing the "activity" type of an object which can either be an activity or
+	// en emotion
+	EntityTypeActivity WhetherActivityOrEmotion = "activity"
+	// EntityTypeEmotion is a constant representing the "emotion" type of an object which can either be an activity or
+	// an emotion
+	EntityTypeEmotion WhetherActivityOrEmotion = "emotion"
+)
+
+// Logical objects
+
 type User struct {
 	Id           int            `db:"id"`
 	FirstName    string         `db:"first_name"`
@@ -25,14 +42,13 @@ type ActivityOrEmotion struct {
 	TypeId     int `db:"type_id"`
 	DayId      int `db:"day_id"`
 	Proportion int `db:"proportion"`
-	// TODO: probably needs a field telling whether it's an activity or an emotion
 }
 
 type ActivityOrEmotionType struct {
-	Id         int    `db:"id"`
-	UserId     int    `db:"user_id"`
-	Name       string `db:"name"`
-	Color      string `db:"color"`
-	IsEveryday bool   `db:"is_everyday"`
-	// TODO: probably needs a field telling whether it's an activity or an emotion
+	Id         int                      `db:"id"`
+	UserId     int                      `db:"user_id"`
+	Name       string                   `db:"name"`
+	Color      string                   `db:"color"`
+	IsEveryday bool                     `db:"is_everyday"`
+	EntityType WhetherActivityOrEmotion `db:"activity_or_emotion"`
 }
