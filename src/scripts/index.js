87e9cf280235ae476acd0665e547125b8296d79b
Tatiana Kadykova <tanya-kta@bk.ru>
2020-12-06 18:57:49 +0300

    Fixed issues found during Codacy checks

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,12 @@
-var day_buttons = new Vue({
-    el:'#day_buttons',
+var dayButtons = new Vue({
+    el: "#dayButtons",
 
     data: {
         days: []
     },
 
     methods: {
-        colorToHex: function (color) {
+        colorToHex(color) {
             function compToHex(c) {
                 let hex = c.toString(16);
                 return hex.length === 1 ? "0" + hex : hex;
@@ -14,50 +14,45 @@ var day_buttons = new Vue({
             return "#" + compToHex(color[0]) + compToHex(color[1]) + compToHex(color[2]);
         },
 
-        getDayUrl: function (day) {
-            return "/" + day.getFullYear() + "_" + day.getMonth() + "_" + day.getDate();
-        },
-
-        isDayFilled: function (day) {
-            for(let i = 0; i < this.days.length; ++i)
-            {
-                if(Date.parse(this.days[i].date) === Date.parse(day.toISOString()))
+        isDayFilled(day) {
+            for(let filledDay of this.days) {
+                if(Date.parse(filledDay.date) === Date.parse(day.toISOString())) {
                     return true;
+                }
             }
             return false;
         },
 
-        getFilledDayColor: function (day) {
-            for(let i = 0; i < this.days.length; ++i)
-            {
-                if(Date.parse(this.days[i].date) === Date.parse(day.toISOString()))
-                    return this.days[i].average_color;
+        getFilledDayColor(day) {
+            for(let filledDay of this.days) {
+                if(Date.parse(filledDay.date) === Date.parse(day.toISOString())) {
+                    return filledDay.average_color;
+                }
             }
             return [169, 169, 169];
         },
 
-        getDisplayedWeeks: function () {
-            let closest_sunday = new Date();
-            closest_sunday = new Date(Date.UTC(closest_sunday.getFullYear(), closest_sunday.getMonth(),
-                closest_sunday.getDate() + 7 - closest_sunday.getDay()));
-            let three_weeks = [];
-            for(let i = 2; i >= 0; --i)
-            {
-                let current_week = [];
-                for(let j = 6; j >= 0; --j)
-                {
-                    current_week.push(new Date(closest_sunday));
-                    current_week[6 - j].setUTCDate(closest_sunday.getUTCDate() - j - i * 7);
+        getDisplayedWeeks() {
+            let closestSunday = new Date();
+            closestSunday = new Date(Date.UTC(closestSunday.getFullYear(), closestSunday.getMonth(),
+                closestSunday.getDate() + 7 - closestSunday.getDay()));
+            let threeWeeks = [];
+            for(let i = 2; i >= 0; --i) {
+                let currentWeek = [];
+                for(let j = 6; j >= 0; --j) {
+                    currentWeek.push(new Date(closestSunday));
+                    currentWeek[6 - j].setUTCDate(closestSunday.getUTCDate() - j - i * 7);
                 }
-                three_weeks.push(current_week);
+                threeWeeks.push(currentWeek);
             }
-            return three_weeks;
+            return threeWeeks;
         }
     },
+
     mounted() {
-        axios.get("/api/days/brief").then(response => {
+        axios.get("/api/days/brief").then((response) => {
             this.days = response.data;
-        })
+        });
     }
 
-})
+});
